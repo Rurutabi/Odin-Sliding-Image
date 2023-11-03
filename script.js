62ce5d13fa29fa62d3c1de095ec0149f11f9c74b
Sira <siraphop_2538@hotmail.com>
2023-11-03 18:28:56 +1100

    Set up foundational project structure

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-"use strict";
+'use strict';
+
+class slideImage {
+  rightArrow = document.querySelector('.right-arrow');
+  leftArrow = document.querySelector('.left-arrow');
+  slideImage = document.querySelectorAll('.img-container');
+
+  constructor() {
+    this._slideRight();
+    this._slideLeft();
+  }
+
+  _slideRight() {
+    this.rightArrow.addEventListener('click', function (e) {
+      e.preventDefault();
+      console.log('right');
+    });
+  }
+
+  _slideLeft() {
+    this.leftArrow.addEventListener('click', function (e) {
+      e.preventDefault();
+      console.log('Left');
+    });
+  }
+}
+
+const slideExample = new slideImage();
