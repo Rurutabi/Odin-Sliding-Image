d2f4a324c89303a24221948692833d0ad8bcb799
Sira <siraphop_2538@hotmail.com>
2023-11-04 21:11:05 +1100

    Slide right now worknig

--- script.js
+++ script.js
@@ -3,26 +3,42 @@
 class slideImage {
   rightArrow = document.querySelector('.right-arrow');
   leftArrow = document.querySelector('.left-arrow');
-  slideImage = document.querySelectorAll('.img-container');
+  slide = document.querySelectorAll('.img-container');
+  pageNumber = document.querySelector('.page-number');
+  counter = 1;
 
   constructor() {
     this._slideRight();
     this._slideLeft();
+    this.printsomething();
   }
+  printsomething() {}
 
   _slideRight() {
-    this.rightArrow.addEventListener('click', function (e) {
+    this.rightArrow.addEventListener('click', e => {
       e.preventDefault();
-      console.log('right');
+      if (this.counter === null) return this.counter;
+
+      if (this.counter < 4) {
+        this.counter++;
+        console.log(this.counter);
+
+        for (const element of this.slide) {
+          element.classList.add('hide');
+          if (element.classList.contains(`img${this.counter}`)) {
+            element.classList.remove('hide');
+          }
+        }
+      }
     });
   }
 
   _slideLeft() {
-    this.leftArrow.addEventListener('click', function (e) {
+    this.leftArrow.addEventListener('click', e => {
       e.preventDefault();
       console.log('Left');
     });
   }
 }
 
-const slideExample = new slideImage();
+const slide1 = new slideImage();
